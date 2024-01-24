0a2fac210b2d7f3793f053ae0a9bb2ececdf19d2
Saurav Khare <saurav.erahk@gmail.com>
2024-01-25 03:00:12 +0530

    Made the loader UI inline with app. - [TODO] Move loader to a separate component.

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -6,7 +6,13 @@ function WeatherWidget() {
 	const data = useContext(WeatherContext);
 
 	if (data.WD.loading) {
-		return <h1>Loading....</h1>;
+		return (
+			<div className="w-4/5 mx-auto mt-24 text-center">
+				<p className="animate-pulse text-5xl text-white font-lora font-semibold">
+					Loading
+				</p>
+			</div>
+		);
 	} else {
 		return (
 			<section className="w-4/5 mx-auto mt-24 mb-10">
